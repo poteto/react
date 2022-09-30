2fe17df4b8fe404b16bde5bbe55cfec2fb98a4d0
Rick Hanlon <rickhanlonii@gmail.com>
2022-09-30 12:06:11 -0400

    Revert "experimental_use(promise) for SSR (#25214)"
    This reverts commit a0abef3e

--- packages/react-server/src/ReactFlightHooks.js
+++ packages/react-server/src/ReactFlightHooks.js
@@ -42,10 +42,8 @@ export function prepareToUseHooksForComponent(
   thenableState = prevThenableState;
 }
 
-export function getThenableStateAfterSuspending(): null | ThenableState {
-  const state = thenableState;
-  thenableState = null;
-  return state;
+export function getThenableStateAfterSuspending() {
+  return thenableState;
 }
 
 function readContext<T>(context: ReactServerContext<T>): T {
@@ -189,9 +187,8 @@ function use<T>(usable: Usable<T>): T {
           }
         }
       }
-    } else if (usable.$$typeof === REACT_SERVER_CONTEXT_TYPE) {
-      const context: ReactServerContext<T> = (usable: any);
-      return readContext(context);
+    } else {
+      // TODO: Add support for Context
     }
   }
 
